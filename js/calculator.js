f11a9120179fda9b78b8d3e61a0f2dfc3491bbf9
Naomi Owens <naomiowens@Naomis-Mac-mini.local>
2015-09-03 20:51:57 -1000

    wrote out some psuedo code for the functionality of the cash register

---
+++ js/calculator.js
@@ -0,0 +1,12 @@
+// gather the input as customer inputs
+// - push items into an array
+// display as customer presses buttons
+// when user presses '=' do the functions
+//
+
+
+// functions the calculator will do:
+// add
+// subtract
+// multiply
+// divide
---
+++ js/cash_register.js
@@ -0,0 +1,34 @@
+// set numbers 0 - 9 to integers (is a number)
+// set operation (is an operator)
+
+// eg customer input 1, 2, +, 3, 4, *, 5, 6, 7,-, 8, 9,=
+// - display input until user presse '='
+
+// once '=' is pressed:
+// determine each integer...
+// number to be calcualted is first array of number
+  // - store into equation array
+      //eg // numToCalculate = [12, 34, 567, 89];
+  // - stop at operator
+    // - store into operator array
+      // eg // operation = [+, *, -, =]
+
+// do the operation
+  // total = numToCalculate[0] + operation[0] + numToCalculate[1] + operation [1] ...
+      // - display total
+
+
+// get the balance (display the total)
+// deposit cash (monies recieved by customer)
+  // - input user keystrokes until user presses operation buttons
+  // - display keystrokes
+  // -
+
+// withdraw cash
+  // - displays the amount user taking out of the register
+
+// clear
+
+
+//operation buttons:
+//  +, -, *, /, =, 'deposit cash',
